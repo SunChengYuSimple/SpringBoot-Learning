0e3b301aba61fe0ce2eaa4ae234b24a9cb2e8dc3
Oliver <n.sunchengyu@gmail.com>
2022-03-03 23:04:10 +0800

    教程SpringBoot2

---
+++ 2.x/chapter1-1/src/main/java/com/didispace/chapter11/HiController.java
@@ -0,0 +1,17 @@
+package com.didispace.chapter11;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 控制器
+ * @author Oliver
+ */
+@RestController
+@RequestMapping("/hiController")
+public class HiController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello Spring Boot 2";
+    }
+}
